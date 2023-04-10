e8b017cf9cd0efd304946a4b9dbfdc27708d3e75
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-04-10 18:24:24 -0300

    Versão alpha

--- index.js
+++ index.js
@@ -74,6 +74,18 @@ function abrirConta() {
             fs.mkdirSync('contas');
         }
 
+        //verifica se o nome do usuario esta em branco
+        if (nomeConta === '') {
+            console.log(chalk.bgRed.black("O seu nome não pode ser ' ', escolha outro nome"));
+            return abrirConta();
+        }
+
+        //verifica se o nome do usuario é uma string e nao é um numero
+        if (typeof nomeConta !== 'string' || !/^[a-zA-Z]+$/.test(nomeConta)) {
+            console.log(chalk.bgRed.black('A sua conta deve ter apenas Letras!'));
+            return abrirConta();
+        }
+
         //verifica se o usuario já existe detro da pasta contas;
         if (fs.existsSync(`contas/${nomeConta}.json`)) {
             console.log(chalk.bgRed.black('Esta conta ja existe, escolha outro nome!'));
@@ -114,6 +126,12 @@ function abrirConta() {
             ])
             .then((answer) => {
                 const senha = answer['senha'];
+
+                //filtro para a senha ter mais de cinco digitos
+                if (senha.length < 5) {
+                    console.log(chalk.bgRed.black('A sua senha deve conter mais de cinco carateres'));
+                    return opcoesSenhaErro();
+                }
                 
                 //Filtrando a senha
                 if (typeof senha !== 'string' || !/^[0-9]+$/.test(senha)) {
@@ -286,23 +304,64 @@ function sacar() {
     .then((answer) => {
         const nomeConta = answer['nomeConta'];
 
-        if(!checarConta(nomeConta)) {
-            return sacar();
+        if (!checarConta(nomeConta)) {
+            console.log('================================================');
+
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'novoNome',
+                    message: 'O que você deseja fazer?',
+                    choices: [
+                        'Escolher um nome para a sua nova conta',
+                        'Voltar ao menu principal',
+                        'Sair',
+                    ],
+                }
+            ])
+            .then((answer) => {
+                const rename = answer['novoNome'];
+
+                if (rename === 'Escolher um nome para a sua nova conta') {
+                    abrirConta();
+                } else if (rename === 'Voltar ao menu principal') {
+                    menu();
+                } else if (rename === 'Sair') {
+                    console.log(chalk.bgBlue.black('Obrigado por usar o Accounts!'));
+                    process.exit();
+                }
+            })
+        } else {
+            inquirer.prompt([
+                {
+                    name: 'senha',
+                    message: 'Digite a sua senha:',
+                }
+            ])
+            .then((answer) => {
+                const senha = Number(answer['senha']);
+                const contaDados = buscarContaJson(nomeConta);
+
+                if (!verificarSenha(nomeConta, senha)) {
+                    console.log(chalk.bgRed.black('Senha incorreta, tente novamente!'));
+                    sacar();
+                } else {
+                    console.log('================================================');
+                    inquirer.prompt([
+                        {
+                            name: 'valor',
+                            message: 'Quanto voçê deseja sacar? R$',
+                        },
+                    ])
+                    .then((answer) => {
+                        const valor = answer['valor'];
+
+                        removerValor(nomeConta, valor);
+                        menu();
+                    })
+                }
+            })
         }
-
-        console.log('================================================');
-        inquirer.prompt([
-            {
-                name: 'valor',
-                message: 'Quanto voçê deseja sacar? R$',
-            },
-        ])
-        .then((answer) => {
-            const valor = answer['valor'];
-
-            removerValor(nomeConta, valor);
-            menu();
-        })
     })
 }
 
@@ -345,42 +404,58 @@ function deletarConta() {
                     process.exit();
                 };
             });
+        } else {
+            console.log('================================================');
+            inquirer.prompt([
+                {
+                    name: 'senha',
+                    message: 'Digite a sua senha',
+                }
+            ])
+            .then((answer) => {
+                const senha = Number(answer['senha']);
+                const contaDados = buscarContaJson(nomeConta);
+
+                if (!verificarSenha(nomeConta, senha)) {
+                    console.log(chalk.bgRed.black('Senha incorreta, tente novamente!'));
+                    deletarConta();
+                } else {
+                    console.log(chalk.bgBlue.black(`A conta ${nomeConta} foi selecionada!`));
+
+                    inquirer.prompt([
+                        {
+                            name: 'deletar',
+                            message: 'Tem certeza? sim = s / não = n  :',
+                        }
+                    ])
+                    .then((answer) => {
+                        const deletar = answer['deletar'];
+
+                        //verificando se a resposta foi algo diferente de s ou n
+                        if(deletar !== 's' && deletar !== 'n') {
+                            console.log(chalk.bgRed.black('Digite uma opção valida!'));
+                            menu();
+                        }
+
+                        if(deletar === 'n') {
+                            console.log(chalk.bgGreen.black('Obrigado por continuar conosco!'));
+                        }
+
+                        //deletando o arquivo da pasta contas
+                        if (deletar === 's') {
+                            console.log(chalk.bgRed.black('sua conta foi deletada!'));
+                            fs.unlink(`contas/${nomeConta}.json`, function (err) {
+                                if (err) throw err;
+                            });
+                        }
+
+                        menu();
+                    })
+                };
+            });
         };
-
-        console.log('================================================');
-        console.log(chalk.bgBlue.black(`A conta ${nomeConta} foi selecionada!`));
-
-        inquirer.prompt([
-            {
-                name: 'deletar',
-                message: 'Tem certeza? sim = s / não = n  :',
-            }
-        ])
-        .then((answer) => {
-            const deletar = answer['deletar'];
-
-            //verificando se a resposta foi algo diferente de s ou n
-            if(deletar !== 's' && deletar !== 'n') {
-                console.log(chalk.bgRed.black('Digite uma opção valida!'));
-                menu();
-            }
-
-            if(deletar === 'n') {
-                console.log(chalk.bgGreen.black('Obrigado por continuar conosco!'));
-            }
-
-            //deletando o arquivo da pasta contas
-            if (deletar === 's') {
-                console.log(chalk.bgRed.black('sua conta foi deletada!'));
-                fs.unlink(`contas/${nomeConta}.json`, function (err) {
-                    if (err) throw err;
-                });
-            }
-
-            menu();
-        })
     });
-}
+};
 
 /* Funções reutilizaveis */
 function checarConta(nomeConta) {
@@ -476,3 +551,4 @@ function verificarSenha(nomeConta, senha) {
         return true;
     };
 };
+
